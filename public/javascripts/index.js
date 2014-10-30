efff379048ec13eabe1f31000a1db8f3427bbb53
Dan Rashid <drashid@signal.co>
2014-10-29 19:59:33 -0500

    Adding multiple instances

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,8 +2,20 @@
 'use strict';
 
 $(function () {
+  $.get('/stats/?max=1000', function (res) {
+    d3.select('#first')
+      .datum(res)
+      .call(foo.chart);
+  });
+
+  $.get('/stats/?max=500', function (res) {
+    d3.select('#second')
+      .datum(res)
+      .call(foo.chart);
+  });
+
   $.get('/stats/', function (res) {
-    d3.select('svg')
+    d3.select('#third')
       .datum(res)
       .call(foo.chart);
   });
--- routes/stats.js
+++ routes/stats.js
@@ -4,13 +4,14 @@ var router = express.Router();
 /* GET stats. */
 router.get('/', function(req, res) {
   var count = +req.query.count || 60,
+    max = +req.query.max || 100,
     now = Math.floor(new Date() / 1000) * 1000,
     data = [];
 
   while (count >= 0) {
     data.push([
       now - count * 1000,
-      Math.round(Math.random() * 100)
+      Math.round(Math.random() * max)
     ]);
     count -= 1;
   }
